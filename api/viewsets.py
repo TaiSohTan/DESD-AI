a0508b2e3715e7b261c0cedd285abb2f9e1eead5
Dexter Zheng Shen Tan <104249150+TaiSohTan@users.noreply.github.com>
2025-03-13 15:27:56 +0000

    Latest

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, permissions
-from .models import User
+from .models import User,Role
+from rest_framework.response import Response
 from .serializers import CustomUserSerializer
 from rest_framework.decorators import action
 
@@ -14,16 +15,16 @@ class UserViewSet(viewsets.ModelViewSet):
         return super().get_permissions()
     
     @action(detail=True, methods=['post'])
-    def assign_role(self, request, pk=None):
+    def update_role(self, request, pk=None):
         user = self.get_object()
         if request.user.role != Role.ADMIN:
             return Response({'error': 'Only Admins can assign roles'}, status=403)
         
         role = request.data.get('role')
-        if role not in Role.values:
+        if role not in [choice[0] for choice in Role.choices]:
             return Response({'error': 'Invalid role'}, status=400)
         
         user.role = role
         user.save()
-        return Response({'message': f'Role updated to {role}'})
+        return Response({'message': f'Role updated to {role}'}, status=200)
 

91a74e587220d2b73bbaf357aa08815029ed22d6
39kash <kashh1309@gmail.com>
2025-04-23 16:27:20 +0100

    latest env, dockerfile and api client

--- utils/ml_api_client.py
+++ utils/ml_api_client.py
@@ -4,7 +4,7 @@ from django.conf import settings
 ## Prediction Endpoint in the FastAPI Server 
 def predict(data):
     """Send a prediction request to the FastAPI service"""
-    url = f"{settings.FASTAPI_BASE_URL}/predict"
+    url = f"http://host.docker.internal:8088/predict"
     try:
         response = requests.post(
             url,
@@ -24,10 +24,12 @@ def predict(data):
 
 def health():
     """Health check endpoint in the FastAPI Server"""
-    url = f"{settings.FASTAPI_BASE_URL}/health"
+    url = f"http://host.docker.internal:8088/health"
     try:
         response = requests.get(url, timeout=5)  # Add timeout
+        print(f"settings.FASTAPI_BASE_URL: {settings.FASTAPI_BASE_URL}")
         return response.status_code == 200
     except requests.exceptions.RequestException as e:
+        print(f"settings.FASTAPI_BASE_URL: {settings.FASTAPI_BASE_URL}")
         print(f"ML API health check failed: {str(e)}")
         return False
